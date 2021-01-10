89b88c12955dfd7cbc5e38eb7ba473424ade4eda
Robin Probst <robinprobst@freenet.de>
2021-01-10 12:53:40 +0100

    erster commit

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+
+int main(void)
+{
+    
+    std::cout << "Hello World\n";
+    
+    int a;
+    for(a=5; a<10; ++a)
+    {
+        std::cout << a << "\n";
+    }
+    
+    
+}
