c32a247c0427d301dc674a227d489ff66d05d4a9
kommilitone2 <bsp@bsp.de>
2021-01-13 11:39:42 +0100

    new commit

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,17 +1,20 @@
-#include <iostream>
-
-//Änderung Nummer1.
-
-int main(void)
-{
-    
-    std::cout << "Hello World\n";
-    
-    int a;
-    for(a=5; a<10; ++a)
-    {
-        std::cout << a << "\n";
-    }
-    
-    
-}
+ #include <iostream>
+ 
+ //Aenderung Nummer2.
+ // change 1 diff
+ int main(void)
+ {
+      
+     std::cout << "Hello World\n";    
+     //change 2 diff
+     int a;
+     for(a=5; a<10; ++a)
+     {
+         std::cout << a << "\n";
+         std::cout << a+4 << "\n";
+     }
+-    
+     // change 3 diff
+     int b;
+     int c;
+ }
